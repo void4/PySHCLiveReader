686f843cf68460150c4fa2540232ca993cfbab5a
void4 <void4@noreply.github.io>
2020-02-15 13:33:19 +0100

    Make search.py more cheat-engine like

--- search.py
+++ search.py
@@ -1,20 +1,42 @@
 from sys import argv
 from pymem import Pymem
+from copy import deepcopy
 
 pm = Pymem("Stronghold_Crusader_Extreme.exe")
 
-target = int(argv[1])
+
 
 start = 0x011F2938
 step = 4
 #V1.2.1-E
 #0x122c760 P1 Gold
-for i in range(100000):
+history = []
+
+def converge(target):
+    #print(history)
+    history.append({})
+    for i in range(100000):
+        try:
+            address = start + i*step
+            value = pm.read_int(address)
+            if value == target:
+                #print("FOUND")
+                if len(history) == 1 or address in history[-2]:
+                    print(hex(address), value)
+                    history[-1][address] = value
+        except UnicodeDecodeError:
+            pass
+
+if len(argv) > 1:
+    target = int(argv[1])
+    converge(target)
+
+while True:
     try:
-        address = start + i*step
-        value = pm.read_int(address)
-        if value == target:
-            #print("FOUND")
-            print(hex(address), address, value)
-    except UnicodeDecodeError:
-        pass
+        inp = input(">")
+        if inp == "q":
+            break
+        target = int(inp)
+    except ValueError:
+        continue
+    converge(target)
