5aa09ac6f5781414396fab1c696a3904171d73af
void4 <void4@noreply.github.io>
2020-02-15 15:07:31 +0100

    Specify UNKNOWN version

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from memory import MemoryReader
 #V1.2.1-E
 V1_2_1_E = """Name:32:s Gold:4 Units:4 Popularity:4 Population:2 Housing:2
 - 0x122c760
-- 0x1230154
-- 0x1233b48
-- 0x123753c
-- 0x123af30
+- 0x1230154 0x1230000
+- 0x1233b48 0x1242348
+- 0x123753c 0x12373e8
+- 0x123af30 0x123addc
 - 0x123e924
 - 0x1242318
 - 0x1245d0c"""
 
-V1_UNKNOWN = """Name:32:s Gold:4 Units:4 Popularity:4 Population:2 Housing:2
+V1_41E_UCP2_13 = """Name:32:s Gold:4 Units:4 Popularity:4 Population:2 Housing:2
 0x024BA286 0x011F2938 0x011F27E4 0x011F2870 0x011F45AC 0x011F24A0
 0x024BA2E0 0x011F632C 0x011F61D8 0x011F6264 0x011F7FA0 0x011F5E94
 0x024BA33A 0x011F9D20 0x011F9BCC 0x011F9C58 0x011FB994 0x011F9888
@@ -32,37 +32,48 @@ import matplotlib.animation as animation
 from matplotlib import style
 import numpy as np
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-xs = []
-ys = [[] for i in range(8)]
+def liveplot():
+	fig = plt.figure()
+	ax = fig.add_subplot(1, 1, 1)
+	xs = []
+	ys = [[] for i in range(8)]
 
-def animate(i):
+	def animate(i):
 
+		tables = reader.runOnce()
+
+		# Limit x and y lists to 20 items
+		#xs = xs[-20:]
+		#ys = ys[-20:]
+
+		# Add x and y to lists
+		xs.append(i)
+		ax.clear()
+		for playerindex in range(8):
+			player = tables["PlayerTable"][playerindex]
+			gold = [m for m in player if m.name=="Gold"][0]
+			ys[playerindex].append(gold.value)
+			ax.plot(xs, ys[playerindex], label=f"Player{playerindex} - {gold.value}")
+
+		# Format plot
+		plt.xticks(rotation=45, ha='right')
+		plt.subplots_adjust(bottom=0.30)
+		plt.title("Game stats")
+		plt.ylabel('Gold')
+		plt.legend()
+		#plt.axis([1, None, 0, 1.1]) #Use for arbitrary number of trials
+		#plt.axis([1, 100, 0, 1.1]) #Use for 100 trial demo
+
+	# Set up plot to call animate() function periodically
+	ani = animation.FuncAnimation(fig, animate, interval=50)
+	plt.show()
+
+from time import sleep
+"""
+while True:
 	tables = reader.runOnce()
-
-	# Limit x and y lists to 20 items
-	#xs = xs[-20:]
-	#ys = ys[-20:]
-
-	# Add x and y to lists
-	xs.append(i)
-	ax.clear()
-	for playerindex in range(8):
-		player = tables["PlayerTable"][playerindex]
-		gold = [m for m in player if m.name=="Gold"][0]
-		ys[playerindex].append(gold.value)
-		ax.plot(xs, ys[playerindex], label=str(playerindex))
-
-	# Format plot
-	plt.xticks(rotation=45, ha='right')
-	plt.subplots_adjust(bottom=0.30)
-	plt.title("Game stats")
-	plt.ylabel('Gold')
-	plt.legend()
-	#plt.axis([1, None, 0, 1.1]) #Use for arbitrary number of trials
-	#plt.axis([1, 100, 0, 1.1]) #Use for 100 trial demo
-
-# Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, interval=50)
-plt.show()
+	print(tables)
+	sleep(1)
+"""
+if __name__ == "__main__":
+	liveplot()
--- search.py
+++ search.py
@@ -4,47 +4,49 @@ from copy import deepcopy
 
 pm = Pymem("Stronghold_Crusader_Extreme.exe")
 
-
-
 start = 0x011F2938
 step = 4
-#V1.2.1-E
-#0x122c760 P1 Gold
+
 history = []
 
 def converge(target):
     #print(history)
     history.append({})
-    for i in range(100000):
+    for i in range(25000000):
         try:
             address = start + i*step
-            value = pm.read_int(address)
-            if value == target:
-                #print("FOUND")
-                if len(history) == 1 or address in history[-2]:
-                    print(hex(address), value)
-                    history[-1][address] = value
+            if len(history) == 1 or address in history[-2]:
+                values = [pm.read_int(address), pm.read_short(address), pm.read_string(address)]
+                if target in values:
+                    #print("FOUND")
+                    print(hex(address), values)
+                    history[-1][address] = values
         except UnicodeDecodeError:
             pass
 
 if len(argv) > 1:
     arg1 = argv[1]
     if arg1 == "read":
-        addr = int(argv[2], 16)
-        print(pm.read_int(addr))
+        addr = eval(argv[2])#int(argv[2], 16)
+        print(hex(addr), pm.read_int(addr), pm.read_short(addr), pm.read_string(addr))
         exit(0)
     try:
-        target = int(arg1)
+        target = eval(arg1)
     except ValueError:
         exit(1)
     converge(target)
 
 while True:
     try:
+        #if inp=="d": import pdb; pdb.set_trace()
         inp = input(">")
         if inp == "q":
             break
-        target = int(inp)
+        elif inp == "<":
+            print("Going back <1")
+            history = history[:-1]
+        else:
+            target = eval(inp)
+            converge(target)
     except ValueError:
         continue
-    converge(target)
